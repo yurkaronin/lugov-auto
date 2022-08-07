c4060194f61bd2de6eb5b0dc245583391bc0bc23
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-08-07 16:12:35 +0300

    адаптив карточки под планшет

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,29 @@ function include(url) {
 include("./js/parts/menu-button.js");
 include("./js/parts/filter-button.js");
 
-include("./js/parts/slider-personal.js");
+// скрипт инициализации слайдера для детальной карточки авто
+if(document.querySelector('.swiper-personal-card')) {
+  let swiperPersonalCard = new Swiper(".swiper-personal-card", {
+    pagination: {
+      el: ".swiper-personal-card .swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-personal-card .swiper-button-next",
+      prevEl: ".swiper-personal-card .swiper-button-prev",
+    },
+  });
+};
 
+let miniature = document.querySelectorAll('.full-card__gallery a');
 
+for (let miniatureItem of miniature) {
+  miniatureItem.onclick = function (e) {
+    e.preventDefault();
+    let test = miniatureItem.dataset.index;
+    console.log(miniatureItem.dataset.index);
+    // console.log('переключаем слайд');
+    // swiperPersonalCard.slideTo(1);
+    swiperPersonalCard.slideTo;
+  }
+}
