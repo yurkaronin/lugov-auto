d1baf17cecd919607654ee2c644a8213d496faa3
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-08-08 13:56:53 +0300

    JS скрипты для слайдеров

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@ include("./js/parts/filter-button.js");
 
 // скрипт инициализации слайдера для детальной карточки авто
 if(document.querySelector('.swiper-personal-card')) {
+  let miniature = document.querySelectorAll('.full-card__gallery a');
+
   let swiperPersonalCard = new Swiper(".swiper-personal-card", {
     pagination: {
       el: ".swiper-personal-card .swiper-pagination",
@@ -27,17 +29,45 @@ if(document.querySelector('.swiper-personal-card')) {
       prevEl: ".swiper-personal-card .swiper-button-prev",
     },
   });
+
+  for (let i = 0; i < miniature.length; i++) {
+    miniature[i].setAttribute('data-index', i);
+    console.log(miniature[i]);
+    miniature[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      swiperPersonalCard.slideTo(event.target.getAttribute('data-index'))
+    })
+  }
 };
 
-let miniature = document.querySelectorAll('.full-card__gallery a');
 
-for (let miniatureItem of miniature) {
-  miniatureItem.onclick = function (e) {
-    e.preventDefault();
-    let test = miniatureItem.dataset.index;
-    console.log(miniatureItem.dataset.index);
-    // console.log('переключаем слайд');
-    // swiperPersonalCard.slideTo(1);
-    swiperPersonalCard.slideTo;
-  }
-}
+if (document.querySelector('.card-item-slider')) {
+  const sliderCards = document.querySelectorAll('.card-item');
+
+
+  for(let i = 0; i < sliderCards.length; i++) {
+    if (sliderCards[i].querySelector('.card-item-slider__container')) {
+      sliderCards[i].querySelector('.card-item-slider__container').classList.add( `card-gallery-${i}`);
+      let swiperTest = new Swiper(`.card-item-slider__container.card-gallery-${i}`, {});
+
+      const arrItemNav = sliderCards[i].querySelectorAll('.card-item-navigation__item');
+
+      for (let i = 0; i < arrItemNav.length; i++) {
+        arrItemNav[i].setAttribute('data-number', i);
+      };
+
+      sliderCards[i].querySelector('.card-item-navigation').addEventListener('mouseover', (event) => {
+        if (event.target.classList.contains('card-item-navigation__item')) {
+          sliderCards[i].querySelectorAll('.card-item-navigation__item').forEach(elem => {
+            elem.classList.remove('card-item-navigation__item--active');
+          });
+
+          event.target.classList.add('card-item-navigation__item--active');
+          swiperTest.slideTo(event.target.getAttribute('data-number'));
+        }
+
+      });
+    }
+  };
+};
+
