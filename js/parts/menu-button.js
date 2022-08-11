49dd9e9bc0507c727773dcfb0e2cedea53524a5a
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-08-11 16:35:45 +0300

    правка скрипта для кнопки фильтра

--- js/parts/menu-button.js
+++ js/parts/menu-button.js
@@ -1,12 +1,16 @@
-menuButton.onclick = function () {
-  if (document.body.classList.contains('js-mob-menu-show')) {
-    document.body.classList.remove('js-mob-menu-show');
-  } else {
-    document.body.classList.add('js-mob-menu-show');
-    document.body.classList.remove('js-mob-filter-show');
-    if(filterButton) {
-      MicroModal.close('modal-filter');
-    }
 
+if(menuButton) {
+  menuButton.onclick = function () {
+    if (document.body.classList.contains('js-mob-menu-show')) {
+      document.body.classList.remove('js-mob-menu-show');
+    } else {
+      document.body.classList.add('js-mob-menu-show');
+      document.body.classList.remove('js-mob-filter-show');
+      if(filterButton) {
+        MicroModal.close('modal-filter');
+      }
+
+    }
   }
 }
+
